ff333c5be74e75e9e6a946bee49836adb2ef07f0
Hunter Skrasek <hunterskrasek@me.com>
2013-12-04 20:10:33 -0600

    Finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Microposts::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  resources :microposts
+  root 'static_pages#index'
+  match '/signup', to: 'users#new', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
 
+  resources :microposts
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
